7063c70bd5bfac57da8581ab5ffab1dc5e2d0179
Jaimie Jellema <github@jaimiejellema.nl>
2019-05-18 12:12:09 +0200

    changed to new tree based labeling

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@
 
 const unsigned int CONST_SIZE_POWER = 8;
 const unsigned int CONST_SIZE = 1 << CONST_SIZE_POWER;
-const unsigned int SIMULATION_SIZE = 1'000'000;
+const unsigned int SIMULATION_BATCH_SIZE = 100'000;
+const unsigned int SIMULATION_BATCH_NUMBER = 10;
 
 typedef double coord_t;
 
@@ -22,7 +23,10 @@ struct treeNode {
 
 treeNode* rootNode;
 treeNode constellation[CONST_SIZE];
-treeNode constellationCopy[CONST_SIZE];
+treeNode constellationJellema[CONST_SIZE];
+treeNode constellationLarsson[CONST_SIZE];
+
+unsigned int lookupTableJellema[CONST_SIZE];
 
 inline double dist(struct treeNode *a, struct treeNode *b) {
     return (a->I - b->I) * (a->I - b->I) + (a->Q - b->Q) * (a->Q - b->Q);
@@ -44,6 +48,10 @@ bool compareQ (treeNode const& lhs, treeNode const& rhs) {
 bool compareQreverse (treeNode const& lhs, treeNode const& rhs) {
     return (lhs.Q > rhs.Q);
 }
+
+/*
+ * Will generate the constellation and put the corresponding constellation points in the constellation array
+ */
 
 void createConstellation() {
     double golden_angle = M_PI * (3 - sqrt(5));
@@ -76,33 +84,6 @@ void createConstellation() {
  * Depending on if two parents higher is either 0 or 1, division is either 01 or 10 respectively.
  */
 
-
-void createMappingSquareAA(std::vector<std::vector<bool>> splits) {
-    for (int i = 0; i < CONST_SIZE_POWER; i++) {
-        int partitions = 1 << i;
-        for (int j = 0; j < partitions; j++) {
-            int size_div = CONST_SIZE / partitions;
-            int start_index = j*size_div;
-            int end_index   = (j+1)*size_div;
-
-
-            if (splits[i][j] == 1) {
-                if (i%2) {
-                    std::sort(constellation + start_index, constellation + end_index, &compareQ);
-                } else {
-                    std::sort(constellation + start_index, constellation + end_index, &compareI);
-                }
-            } else {
-                if (i%2) {
-                    std::sort(constellation + start_index, constellation + end_index, &compareQreverse);
-                } else {
-                    std::sort(constellation + start_index, constellation + end_index, &compareIreverse);
-                }
-            }
-        }
-    }
-}
-
 treeNode* buildTree(treeNode nodes[], int depth, bool invertI, bool invertQ) {
     size_t size = CONST_SIZE >> depth;
     size_t halfWayIndex = (size - 1) / 2;
@@ -133,17 +114,21 @@ treeNode* buildTree(treeNode nodes[], int depth, bool invertI, bool invertQ) {
 
     if (depth % 2 == 0) {
         if (invertI) {
+            std::cout << "Test: 1"<< std::endl;
             node->leave[1] = buildTree(leftNodes,  depth + 1, invertI, invertQ);
             node->leave[0] = buildTree(rightNodes, depth + 1, !invertI, invertQ);
         } else {
+            std::cout << "Test: 2"<< std::endl;
             node->leave[0] = buildTree(leftNodes,  depth + 1, !invertI, invertQ);
             node->leave[1] = buildTree(rightNodes, depth + 1, invertI, invertQ);
         }
     } else {
         if (invertQ) {
+            std::cout << "Test: 3"<< std::endl;
             node->leave[1] = buildTree(leftNodes,  depth + 1, invertI, invertQ);
             node->leave[0] = buildTree(rightNodes, depth + 1, invertI, !invertQ);
         } else {
+            std::cout << "Test: 4"<< std::endl;
             node->leave[0] = buildTree(leftNodes,  depth + 1, invertI, !invertQ);
             node->leave[1] = buildTree(rightNodes, depth + 1, invertI, invertQ);
         }
@@ -173,15 +158,14 @@ void numberTree(treeNode* node) {
 
 /*
  * Perform a nearestNeighbourhood search using the constellation tree that was build.
- * TODO: implement proper nearest neighbour search
  */
-int nearestNeightbour(coord_t I, coord_t Q) {
+int nearestNeightbour(treeNode nodes[], coord_t I, coord_t Q) {
     double currentBestDistance = 10;
     int indexBest = 0;
 
     for (int i = 0; i < CONST_SIZE; i++) {
-        double diffI = constellation[i].I - I;
-        double diffQ = constellation[i].Q - Q;
+        double diffI = nodes[i].I - I;
+        double diffQ = nodes[i].Q - Q;
         double distance =  diffI * diffI + diffQ * diffQ;
         if (distance < currentBestDistance) {
             indexBest = i;
@@ -190,45 +174,13 @@ int nearestNeightbour(coord_t I, coord_t Q) {
     }
 
     return indexBest;
-};
-
-int visited;
-
-
-//TODO: this function is broken as fuck due to not giving I and Q values to the tree intermediates
-void nearest(struct treeNode *root, struct treeNode *nd, int i,
-             struct treeNode **best, double *best_dist) {
-    double d, dx, dx2;
-
-    if (!root) {
-        return;
-    }
-    d = dist(rootNode, nd);
-    if (i == 0) {
-        dx = root->I - nd->I;
-    } else {
-        dx = root->Q - nd->Q;
-    }
-    dx2 = dx * dx;
-
-    visited++;
-
-    if (!*best || d < *best_dist) {
-        *best_dist = d;
-        *best = root;
-    }
-
-    i ^= 0x01;
-
-    nearest(dx > 0 ? root->leave[0] : root->leave[1], nd, i, best, best_dist);
-
-    if (dx2 >= *best_dist) {
-        return;
-    }
-
-    nearest(dx > 0 ? root->leave[1] : root->leave[0], nd, i, best, best_dist);
 }
 
+void generateLookup(treeNode nodes[]) {
+    for (int i = 0; i < CONST_SIZE; i++) {
+        lookupTableJellema[i] = nearestNeightbour(nodes, constellation[i].I, constellation[i].Q);
+    }
+}
 
 
 //Thanks Brian!
@@ -243,37 +195,9 @@ int hammingDist(unsigned int x, unsigned int y) {
 }
 
 
-std::vector<std::vector<bool>> splitGenerator() {
-    std::vector<std::vector<bool>> splits;
-
-    for (int i = 0; i < CONST_SIZE_POWER; i++) {
-        splits.emplace_back(std::vector<bool>());
-    }
-
-    splits[0].push_back(true);
-    splits[1].push_back(true);
-    splits[1].push_back(true);
-
-    for (int i = 2; i < CONST_SIZE_POWER; i++) {
-        int j = 0;
-        while (j < splits[i-2].size()) {
-            if (splits[i-2][j] == 1) {
-                splits[i].push_back(false);
-                splits[i].push_back(false);
-                splits[i].push_back(true);
-                splits[i].push_back(true);
-            } else {
-                splits[i].push_back(true);
-                splits[i].push_back(true);
-                splits[i].push_back(false);
-                splits[i].push_back(false);
-            }
-            j = j + 1;
-        }
-
-    }
-    return splits;
-}
+/*
+ * Be carefull this is NOT the mapping from Larsson to me, this was just a testing thing
+ */
 
 void printTree(treeNode* node) {
     if (node->leave[0]) {
@@ -288,33 +212,37 @@ void printTree(treeNode* node) {
         std::cout << std::bitset<CONST_SIZE_POWER>(node->index) << " Original " << constellation[node->index].I << " " << constellation[node->index].Q << std::endl;
         std::cout << std::bitset<CONST_SIZE_POWER>(node->index) << " Tree     " << node->I << " " << node->Q << std::endl;
     }
-
-
 }
 
 int main() {
     createConstellation();
     std::cout << "creating mapping" << std::endl;
-    std::copy(std::begin(constellation), std::end(constellation), std::begin(constellationCopy));
-
-    createMappingSquareAA(splitGenerator());
-
-    //std::copy(std::begin(constellation), std::end(constellation), std::begin(constellationCopy));
+    std::copy(std::begin(constellation), std::end(constellation), std::begin(constellationJellema));
+    std::copy(std::begin(constellation), std::end(constellation), std::begin(constellationLarsson));
 
     std::cout << "building tree" << std::endl;
-    rootNode = buildTree(constellationCopy, 0, false, false);
+    rootNode = buildTree(constellationJellema, 0, false, false);
     rootNode->index = 0;
     std::cout << "numbering nodes" << std::endl;
     numberTree(rootNode);
-    std::cout << "printing nodes" << std::endl;
-    printTree(rootNode);
+
+    std::cout << "building tree" << std::endl;
+    rootNode = buildTree(constellationLarsson, 0, false, false);
+    rootNode->index = 0;
+    std::cout << "numbering nodes" << std::endl;
+    numberTree(rootNode);
+
+    generateLookup(constellationJellema);
+
+    //std::cout << "printing nodes" << std::endl;
+    //printTree(rootNode);
 
 
     std::cout << "simulating" << std::endl;
 
     const double mean = 0.0;
     const double stddev = 0.2;
-    std::default_random_engine generator(std::random_device{}());
+    std::mt19937_64 generator(std::random_device{}());
     std::normal_distribution<double> distributionNormal(mean, stddev);
     std::uniform_int_distribution<int> distributionUniform(0, CONST_SIZE - 1);
 
@@ -326,31 +254,17 @@ int main() {
         int nodeIndex = distributionUniform(generator);
 
         treeNode p = constellation[nodeIndex];
+
         double receivedI = p.I + distributionNormal(generator);
         double receivedQ = p.Q + distributionNormal(generator);
 
-        int closests = nearestNeightbour(receivedI, receivedQ);
-
-        //TODO: nearest neighbour using KD tree is broken see function description.
-        /*
-        treeNode test;
-        test.I = receivedI;
-        test.Q = receivedQ;
-        double best_dist;
-        struct treeNode *found;
-        found = nullptr;
-
-        nearest(rootNode, &test, 0, &found, &best_dist);
-        int closestsFast = found->index;
-
-        std::cout << found->I << " " << constellation[closests].I << std::endl;
-        std::cout << found->Q << " " << constellation[closests].Q << std::endl;
-        */
-
+        int closests = nearestNeightbour(constellation, receivedI, receivedQ);
+        
         if (closests != nodeIndex) {
             errorCountSymbol++;
-            errorCountBitJellema += hammingDist(closests, nodeIndex);
-            errorCountBitLarsson += hammingDist(constellation[closests].index, constellation[nodeIndex].index);
+            errorCountBitLarsson += hammingDist(closests, nodeIndex);
+            errorCountBitJellema += hammingDist(lookupTableJellema[closests], lookupTableJellema[nodeIndex]);
+
         }
     }
 
